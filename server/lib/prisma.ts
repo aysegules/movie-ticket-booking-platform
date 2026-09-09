3a9b50be3e97ce615f02518debb100940c842c8b
aysegul <ayseguleskk@gmail.com>
2026-09-09 15:30:19 +0300

    feat(prisma): update PrismaClient initialization and add database connection functions

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,10 +1,30 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client.ts";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+  adapter,
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["error", "warn", "query"]
+      : ["error"],
+});
 
-export { prisma };
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log("Database connected via Prisma");
+  } catch (error) {
+    const message = error instanceof Error ? error.message : String(error);
+    console.error(`Database connection error : ${message}`);
+    process.exit(1);
+  }
+};
+
+const disconnectDB = async () => {
+  await prisma.$disconnect();
+};
+export { prisma, connectDB, disconnectDB };
